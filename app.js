382da1bf2698c7d41e911cc0c4801d8575660cc4
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2014-10-23 12:04:08 -0400

    adapting style to the idiomaticjs

--- app.js
+++ app.js
@@ -1,80 +1,79 @@
-
 /**
  * Created by wilson.campusano on 10/14/2014.
  */
 var lists = {
-    todo: document.getElementById('todo'),
-    done: document.getElementById('done')
+	todo: document.getElementById( 'todo' ),
+	done: document.getElementById( 'done' )
 };
-var onClickCheckBox = function onClickCheckBox(event){
-    var liElement = event.target.parentNode;
-    var list =lists[liElement.parentNode.id === 'todo' ? 'done':'todo'];
-    list.appendChild(liElement);
+var onClickCheckBox = function onClickCheckBox( event ) {
+	var liElement = event.target.parentNode;
+	var list = lists[liElement.parentNode.id === 'todo' ? 'done' : 'todo'];
+	list.appendChild( liElement );
 };
 
 var giveMeInputElement = function giveMeInputElement() {
-    return document.getElementById('inputText');
+	return document.getElementById( 'inputText' );
 };
-var giveMeAddButton = function giveMeAddButton(){
-    return document.getElementById('btnAdd');
-};
-
-var giveMeErrorMessageElement = function giveMeErrorMessageElement(){
-    return document.getElementById('errorMessage');
+var giveMeAddButton = function giveMeAddButton() {
+	return document.getElementById( 'btnAdd' );
 };
 
-var showErrorMessage = function showErrorMessage (){
-    var errorElement = giveMeErrorMessageElement();
-    errorElement.removeAttribute('hidden');
+var giveMeErrorMessageElement = function giveMeErrorMessageElement() {
+	return document.getElementById( 'errorMessage' );
 };
 
-var hideErrorMessage = function hideErrorMessage(){
-    var errorElement = giveMeErrorMessageElement();
-    errorElement.setAttribute('hidden', 'hidden');
+var showErrorMessage = function showErrorMessage() {
+	var errorElement = giveMeErrorMessageElement();
+	errorElement.removeAttribute( 'hidden' );
 };
 
-var onClick = function onClick(){
-    var input = giveMeInputElement();
-    var text = input.value.trim();
-    if(text === ''){
-        showErrorMessage();
-        return;
-    }
-    hideErrorMessage();
-    var task = makeTask(text, onClickCheckBox);
-    lists.todo.appendChild(task);
-    input.value = '';
-    input.focus();
-};
-var onKeyPress = function onKeyPress(event) {
-    var keyCode = event.keyCode;
-    var ENTER_CODE = 13;
-    if(keyCode === ENTER_CODE){
-        onClick();
-    }
+var hideErrorMessage = function hideErrorMessage() {
+	var errorElement = giveMeErrorMessageElement();
+	errorElement.setAttribute( 'hidden', 'hidden' );
 };
 
-var makeTask = function makeTask(str, onClick){
-    var li = document.createElement('li');
-    var checkbox = document.createElement('input');
-    var label = document.createElement('label');
+var onClick = function onClick() {
+	var input = giveMeInputElement();
+	var text = input.value.trim();
+	if ( text === '' ) {
+		showErrorMessage();
+		return;
+	}
+	hideErrorMessage();
+	var task = makeTask( text, onClickCheckBox );
+	lists.todo.appendChild( task );
+	input.value = '';
+	input.focus();
+};
+var onKeyPress = function onKeyPress( event ) {
+	var keyCode = event.keyCode;
+	var ENTER_CODE = 13;
+	if ( keyCode === ENTER_CODE ) {
+		onClick();
+	}
+};
 
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('click',onClick);
-    label.textContent = str;
+var makeTask = function makeTask( str, onClick ) {
+	var li = document.createElement( 'li' );
+	var checkbox = document.createElement( 'input' );
+	var label = document.createElement( 'label' );
 
-    li.appendChild(checkbox);
-    li.appendChild(label);
+	checkbox.type = 'checkbox';
+	checkbox.addEventListener( 'click', onClick );
+	label.textContent = str;
 
-    return li;
+	li.appendChild( checkbox );
+	li.appendChild( label );
+
+	return li;
 };
 
 
-(function(){
-    var btnAdd  = giveMeAddButton();
-    var inputText = giveMeInputElement();
+(function () {
+	var btnAdd = giveMeAddButton();
+	var inputText = giveMeInputElement();
 
-    btnAdd.addEventListener('click', onClick);
-    inputText.addEventListener('keypress', onKeyPress);
-    inputText.focus();
+	btnAdd.addEventListener( 'click', onClick );
+	inputText.addEventListener( 'keypress', onKeyPress );
+	inputText.focus();
 })();
